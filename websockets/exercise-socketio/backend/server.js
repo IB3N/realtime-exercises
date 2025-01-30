166465f1dcfb5c0bbf91240385e978c69c2d5f11
IB3N <benpearce9@live.co.uk>
2025-01-30 11:37:14 +0530

    socketio

--- websockets/exercise-socketio/backend/server.js
+++ websockets/exercise-socketio/backend/server.js
@@ -1,6 +1,6 @@
 import http from "http";
-import handler from "serve-handler";
 import nanobuffer from "nanobuffer";
+import handler from "serve-handler";
 import { Server } from "socket.io";
 
 const msg = new nanobuffer(50);
@@ -19,11 +19,25 @@ const server = http.createServer((request, response) => {
   });
 });
 
-/*
- *
- * Code goes here
- *
- */
+const io = new Server(server, {});
+
+io.on("connection", (socket) => {
+  console.log("connected to " + socket.id);
+  socket.emit("msg:get", { msg: getMsgs() });
+
+  socket.on("disconnected", () => {
+    console.log("disconnected from " + socket.id);
+  });
+
+  socket.on("msg:post", (data) => {
+    msg.push({
+      user: data.user,
+      text: data.text,
+      time: Date.now(),
+    });
+    io.emit("msg:get", { msg: getMsgs() });
+  });
+});
 
 const port = process.env.PORT || 8080;
 server.listen(port, () =>
